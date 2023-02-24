aea032d06fc57223dbc4d2996f07cb640cdddaf1
Sunart1 <79371501+Sunart1@users.noreply.github.com>
2023-02-23 19:21:55 -0500

    Push initial code

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import { Server } from 'socket.io';
+
+const expressApp = express(); //Environment setup
+const PORT = 5050;
+
+expressApp.use(express.json()) //Middlewares
+expressApp.use('/app', express.static('public')); //Middlewares entregamos app al cliente
+
+// expressApp.listen(PORT);
+const httpServer = expressApp.listen(PORT, () => { //Start the server
+    console.log(`http://localhost:${PORT}/app`);
+})
+
+const io = new Server(httpServer, { path: '/real-time' }); //WebSocket Server (instance) initialization
+
+io.on('connection', (socket) => { //Listening for webSocket connections
+    console.log(socket.id)
+
+    socket.on('positions', (message)=>{
+        socket.broadcast.emit('display-positions', message);
+    })
+});
---
+++ public/sketch.js
@@ -0,0 +1,121 @@
+let socket = io("http://localhost:5050", { path: '/real-time' })
+let canvas;
+let controllerX, controllerY = 0;
+let qrCodeX, qrCodeY = 0;
+let mazeImg, snackImg;
+//Interfaces
+let initial, instructions, game, form, thanks;
+
+
+function preload() {
+    mazeImg = loadImage('/app/assets/maze.png');
+    snackImg = loadImage('/app/assets/minisnack.png');
+    initial = loadImage('/app/assets/Initial.jpg');
+    instructions = loadImage('/app/assets/Instructions.jpg')
+    game = loadImage('/app/assets/Game.jpg')
+    form = loadImage('/app/assets/Winner.jpg')
+    thanks = loadImage('/app/assets/thanks.jpg')
+}
+
+
+function setup() {
+    frameRate(60);
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.style('z-index', '-1');
+    canvas.style('position', 'fixed');
+    canvas.style('top', '0');
+    canvas.style('right', '0');
+    controllerX = windowWidth / 2;
+    controllerY = 50;
+    qrCodeX = windowWidth/2 + 5;
+    qrCodeY = windowHeight/2 + 80;
+    imageMode(CENTER);
+    rectMode(CENTER);
+}
+
+function draw() {
+    switch (screen) {
+        case 0:
+            background(255);
+            rect(qrCodeX, qrCodeY, 150, 150);
+            image(initial, windowWidth/2, windowHeight/2);
+            break;
+
+        case 1:
+            background(255);
+            image(instructions, windowWidth/2, windowHeight/2)
+            break;    
+
+        case 2:
+            background(0,95);
+            newCursor(pmouseX, pmouseY);
+            image(snackImg, controllerX, controllerY, 150, 150)
+            image(mazeImg, windowWidth/2, windowHeight/2);
+            break;
+
+        case 3:
+            background(255);
+            image(form, windowWidth/2, windowHeight/2)
+            break;
+        
+        case 4:
+            background(255);
+            image(thanks, windowWidth/2, windowHeight/2)
+            break;
+
+    
+        default:
+            screen = 0;
+            break;
+    }
+
+    changeScreen();
+}
+
+function changeScreen(){
+    if (screen === 0 && dist(pmouseX, pmouseY, qrCodeX, qrCodeY) < 75) {
+        screen = 1;
+    } 
+    
+    if (screen === 1) {
+        if (keyCode === RIGHT_ARROW) {
+            screen = 2;
+        }
+    }
+
+    if (screen === 2) {
+        if(pmouseY > 500){
+            screen = 3;
+        }
+    }
+
+    if (screen === 3) {
+        if (pmouseX > 600) {
+            screen = 4;
+        }
+    }
+}
+
+
+function mouseDragged() {
+    let msn = {controlX : pmouseX, controlY: pmouseY}
+    socket.emit('positions', msn);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function newCursor(x, y) {
+    noStroke();
+    fill(225, 19, 131);
+    ellipse(x, y, 10, 10);
+}
+
+//Add an .on() event on the socket
+
+socket.on('display-positions', msn => {
+    let {controlX, controlY} = msn;
+    controllerX = controlX
+    controllerY = controlY
+})
